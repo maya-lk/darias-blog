5bef2ecfe4d94d457fbe43749e12addd7049b233
hettipower <hettipower@gmail.com>
2019-08-28 09:08:18 +0530

    Updates

---
+++ precache-manifest.927b9236f5d6627d3ed6590c7ed6708a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b507fe063aa9e58a9c077e774d1492",
+    "url": "/darias-blog/index.html"
+  },
+  {
+    "revision": "c605eb498000b1cc5e11",
+    "url": "/darias-blog/static/css/2.bf073607.chunk.css"
+  },
+  {
+    "revision": "cbb8a21eeecae36fcaa2",
+    "url": "/darias-blog/static/css/main.79b14a73.chunk.css"
+  },
+  {
+    "revision": "c605eb498000b1cc5e11",
+    "url": "/darias-blog/static/js/2.47e9dc10.chunk.js"
+  },
+  {
+    "revision": "cbb8a21eeecae36fcaa2",
+    "url": "/darias-blog/static/js/main.b98a259b.chunk.js"
+  },
+  {
+    "revision": "518c4b3c509548241d2a",
+    "url": "/darias-blog/static/js/runtime~main.fa0cf740.js"
+  }
+]);
