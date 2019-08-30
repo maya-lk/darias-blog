c3fbed939f3cefc68faac152145e0fb59bf4fcd5
hettipower <hettipower@gmail.com>
2019-08-30 15:58:43 +0530

    Create Things i do components

--- src/components/homepage/things-i-do.js
+++ src/components/homepage/things-i-do.js
@@ -1,17 +1,71 @@
 import React, { Component } from "react";
+import API from '../../lib/api';
+
+//Child Components
+import ThingsTaxContent from './things/ThingsTaxContent';
+import ThingsContent from './things/ThingsContent';
 
 class ThingsIDo extends Component {
 
-    constructor(props, context){
-        super(props, context)
+    constructor(){
+        super()
         this.state = {
+            thingsTax : {},
+            termImage : '',
+            termID : '',
+            thingsList : {}
         }
-        
-      }
+
+        this.OnClickThings = this.OnClickThings.bind(this);
+    }
+
+    componentDidMount(){
+        //Get Things Taxonomy List
+        API.get('wp/v2/things-tax?orderby=id')
+        .then(data => this.setState({
+            thingsTax : data.data
+        }))
+        .catch(error => console.log(error))
+
+        API.get(`wp/v2/things-do?things-tax=${this.state.termID}`)
+        .then(data => this.setState({
+            thingsList : data.data
+        }))
+        .catch(error => console.log(error))
+
+    }
+
+    OnClickThings(obj , e){
+
+        this.setState({
+            termImage : obj.image,
+            termID : obj.id
+        });
+
+        document.getElementsByClassName("taxTitle")[0].innerHTML = obj.name;
+        document.getElementsByClassName("taxDescription")[0].innerHTML = obj.description;
+
+        var childElements = document.getElementById("termList").childNodes;
+        for (var i = 0; i < childElements.length; i++) {
+            childElements[i].classList.remove('active');
+        }
+        e.target.classList.add('active');
+
+    }
 
     render() {
+        console.log('thingsList' , this.state.thingsList);
+        console.log('termID' , this.state.termID);
         return (
-            <section className="sectionWrap" id="thingsSecWrap">         
+            <section className="sectionWrap d-flex justify-content-between flex-wrap" id="thingsSecWrap">
+                <h3 className="mainTitle">Things i do</h3>
+                <ThingsTaxContent 
+                    taxonomyList={this.state.thingsTax}
+                    OnClickThings={this.OnClickThings}
+                    termSingleImg={this.state.termImage}
+                />
+                <ThingsContent thingsList={this.state.thingsList}/>
+                <div className="bottomBar"></div>
             </section>
         )
     }
---
+++ src/components/homepage/things/ThingsContent.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class ThingsContent extends Component {
+
+    constructor(props, context){
+        super(props, context)
+        this.state = {
+        }
+        
+      }
+
+    render() {
+        return (
+            <div className="thingsDoContentWrap"></div>
+        )
+    }
+
+}
+
+export default ThingsContent;
